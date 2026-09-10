5ff4f610871fd1fdb71e25c00205e331edb9bc50
Quanpm5198 <vinnypm174@gmail.com>
2026-09-10 11:42:20 -0500

    Enhance main.cpp with string and cost output
    Added string handling and cost calculation.

--- input and output/main.cpp
+++ input and output/main.cpp
@@ -1,13 +1,27 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 #include<cmath>
+#include<string>
 
+using namespace std; 
 
 int main()
 {
 	int angstromsPerMeter = pow(10,10);
-	std::cout << angstromsPerMeter;
-	
-	
+    std::cout << angstromsPerMeter;   
+    string text = " Vinny";
+    string text = "Johnny";
 
-	return 0;
-}
+    std::cout << " Length of string: " << text << "\n";
+    
+    
+    double thoseShoesCost = 19.99; 
+    double thosePantsCost = 2690.01;
+
+    double clothesTotalCost = thoseShoesCost + thosePantsCost;
+
+    cout << "Total cost is: $ " << clothesTotalCost << "\n";
+
+    return 0;
